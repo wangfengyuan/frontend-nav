3ca8c200eabe09229459b4968b494237fa900260
wangfengyuan <644301374@qq.com>
2023-05-29 22:00:56 +0800

    feat: add auth api route

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  debug: process.env.NODE_ENV !== "production",
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
